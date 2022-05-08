caf81a6c8e5c98ab71516893d4ac3cae9ae7b41a
hikaru <alclimb@users.noreply.github.com>
2022-05-08 18:08:36 +0900

    feat: ブログのiframe埋め込み対応

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-// canvasを取得
-const myCanvas = document.querySelector<HTMLCanvasElement>('#myCanvas')!;
+// 描画先の要素
+const appElement = document.querySelector<HTMLDivElement>('#appElement')!;
 
 //
 // ➊ three.jsセットアップ
@@ -17,15 +17,18 @@ const scene = new THREE.Scene();
 scene.add(axes);
 
 // カメラを初期化
-const camera = new THREE.PerspectiveCamera(50, myCanvas.offsetWidth / myCanvas.offsetHeight);
+const camera = new THREE.PerspectiveCamera(50, appElement.offsetWidth / appElement.offsetHeight);
 camera.position.set(1, 1, 1);
 camera.lookAt(scene.position);
 
 // レンダラーの初期化
-const renderer = new THREE.WebGLRenderer({ canvas: myCanvas });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor(0xffffff, 1.0); // 背景色
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(myCanvas.offsetWidth, myCanvas.offsetHeight);
+renderer.setSize(appElement.offsetWidth, appElement.offsetHeight);
+
+// レンダラーをDOMに追加
+appElement.appendChild(renderer.domElement);
 
 // カメラコントローラー設定
 const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -34,6 +37,17 @@ orbitControls.minDistance = 0.1;
 orbitControls.maxDistance = 100;
 orbitControls.autoRotate = true;     // カメラの自動回転設定
 orbitControls.autoRotateSpeed = 1.0; // カメラの自動回転速度
+
+// 画面のサイズ変更イベントを登録
+window.addEventListener(`resize`, () => {
+  // レンダラーのサイズを調整
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(appElement.offsetWidth, appElement.offsetHeight);
+
+  // カメラのアスペクト比を調整
+  camera.aspect = appElement.offsetWidth / appElement.offsetHeight;
+  camera.updateProjectionMatrix();
+});
 
 // ➋ 描画ループを開始
 renderer.setAnimationLoop(() => {
